b259aadc02f478a596720897134ffa262ce1fc4e
townwang <townwang@retronix.com.tw>
2020-01-08 15:20:03 +0800

    fixed build issue when HOST is ubuntu18.04 and crosscompile is hf mode.

--- build.sh
+++ build.sh
@@ -5,6 +5,66 @@ set -e
 TOP=`pwd`
 OUT=${TOP}/out
 
+function lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+function check_version()
+{
+OS=`lowercase \`uname\``
+KERNEL=`uname -r`
+MACH=`uname -m`
+
+if [ "{$OS}" == "windowsnt" ]; then
+    OS=windows
+elif [ "{$OS}" == "darwin" ]; then
+    OS=mac
+else
+    OS=`uname`
+    if [ "${OS}" = "SunOS" ] ; then
+        OS=Solaris
+        ARCH=`uname -p`
+        OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+    elif [ "${OS}" = "AIX" ] ; then
+        OSSTR="${OS} `oslevel` (`oslevel -r`)"
+    elif [ "${OS}" = "Linux" ] ; then
+        if [ -f /etc/redhat-release ] ; then
+            DistroBasedOn='RedHat'
+            DIST=`cat /etc/redhat-release |sed s/\ release.*//`
+            PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/SuSE-release ] ; then
+            DistroBasedOn='SuSe'
+            PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+            REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+        elif [ -f /etc/mandrake-release ] ; then
+            DistroBasedOn='Mandrake'
+            PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/debian_version ] ; then
+            DistroBasedOn='Debian'
+            DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
+            PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+            REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+        fi
+        if [ -f /etc/UnitedLinux-release ] ; then
+            DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+        fi
+        OS=`lowercase $OS`
+        DistroBasedOn=`lowercase $DistroBasedOn`
+        readonly OS
+        readonly DIST
+        readonly DistroBasedOn
+        readonly PSUEDONAME
+        readonly REV
+        readonly KERNEL
+        readonly MACH
+    fi
+
+fi
+	
+}
+
 #### Cross compiler tool path ####################################################
 if [ -z $CROSS_COMPILE_PATH ];then
     [ -d /opt/freescale/usr/local ] && CROSS_COMPILE_PATH=/opt/freescale/usr/local
@@ -49,6 +109,19 @@ fi
 
 CROSS_COMPILE=${CROSS_COMPILE_PATH}/${CROSS_COMPILE_TOOL}/${CROSS_COMPILE_GCC}
 
+#### Fix compile error when HOST is ubuntu18.04 and Cross compile toos is hf mode.####
+if [ $(echo ${CROSS_COMPILE_GCC} | grep hf- | wc -l) = 1 ]; then
+	check_version
+	echo "REV=${REV}"
+	if [ ${REV} == "18.04" ]; then
+		export LC_ALL=C
+	else
+		unset LC_ALL
+	fi
+else
+	unset LC_ALL
+fi 
+
 ########################################################################
 export ARCH=arm
 export CROSS_COMPILE=${CROSS_COMPILE}
@@ -65,10 +138,10 @@ fi
 
 #### Target Customer Project ###########################################
 #TARGET_CUSTOMER="RTX-A6"
-TARGET_CUSTOMER="RTX-A6Plus"
+#TARGET_CUSTOMER="RTX-A6Plus"
 #TARGET_CUSTOMER="RTX-Q7"
 #TARGET_CUSTOMER="RTX-PITX-B10"
-#TARGET_CUSTOMER="RTX-PITX-B21"
+TARGET_CUSTOMER="RTX-PITX-B21"
 #TARGET_CUSTOMER="RTX-PITX-B21-AWS"
 #TARGET_CUSTOMER="ADLINK-ABB"
 #TARGET_CUSTOMER="AcBel-VPP"
